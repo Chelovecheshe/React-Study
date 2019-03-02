fa7604b5673d21538222e6df260bbb0f91ebbd08
Ilya <yailyab@gmail.com>
2019-03-02 12:10:16 +0100

    .css files removed

--- src/themes/globalStyle.js
+++ src/themes/globalStyle.js
@@ -1,11 +1,19 @@
 import { createGlobalStyle } from "styled-components";
 
 export default createGlobalStyle`
+
   * {
     font-size: 14px;
     box-sizing: border-box;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
     'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans',
     'Droid Sans', 'Helvetica Neue', sans-serif;  
+  }
+  
+  body {
+    margin: 0;
+    padding: 0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `;
